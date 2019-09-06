7a9811a3e1af092df4216aeefcfdc772368f69d9
sooshe <sooshe@sooshebot.com>
2019-09-06 17:43:34 -0500

    attempted stretch

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -10,19 +10,8 @@
 
 const newTab = document.querySelector('.topics');
 
-axios.get('https://lambda-times-backend.herokuapp.com/topics')
-.then(response => {
-  console.log(response);
-  newTab.appendChild(newtabCreator(response.data));
-  })
-.catch(error => {
-  console.log("The data was not returned", error);
-})
-
-
 function newtabCreator(data){
     const tab = document.createElement('div');
-    
 //set class names
 tab.classList.add('tab');
 
@@ -33,16 +22,17 @@ return tab;
 
 }
 
-//display tabs stretch goal
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+.then(response => {
+  console.log(response);
+  newTab.appendChild(newtabCreator(response.data.topics));
+  // console.log(newTab);
+  
+  })
+.catch(error => {
+  console.log("The data was not returned", error);
+})
 
-// const displayAllTabs = document.querySelector('.topics');
-// function displayAll(data){
-//     const allTabs = document.createElement('div');
 
-// allTabs.classList.add('allTabs');
-// allTabs.textContent = data.topics
-
-// return allTabs;
-// }
-
-
+//display all tabs stretch goal steps
+//pull the nodelist of the data container using querySelectAll
